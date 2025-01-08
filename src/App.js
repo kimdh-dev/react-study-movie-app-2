f5ebdb29d8762dab88a10beaf10e416b366bd603
kimdh <kdh.devs@gmail.com>
2025-01-08 16:48:52 +0900

    25/01/08 ToDo앱 실습

--- src/App.js
+++ src/App.js
@@ -1,74 +1,36 @@
-import Button from "./Button.js";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-/*
-function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue" />
-    </div>
-  );
-}
-*/
-
-/*
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {
-    setCounter((prev) => prev + 1);
-  };
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  useEffect(() => {
-    console.log("only one call");
-  }, []);
-  useEffect(() => {
-    console.log("keyword changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("counter changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("keyword and counter changes");
-  }, [keyword, counter]);
-  return (
-    <div>
-      <input
-        type="text"
-        value={keyword}
-        placeholder="keyword"
-        onChange={onChange}
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
-}
-
-*/
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const Hello = () => {
-    useEffect(() => {
-      console.log("hi");
-      return () => {
-        console.log("bye");
-      };
-    }, []);
-    return <h1>Hello</h1>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((prev) => [toDo, ...prev]);
+    setToDo("");
   };
-  const onClick = () => {
-    setShowing((curr) => !curr);
-  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="todo list type here"
+          value={toDo}
+          onChange={onChange}
+        />
+        <button>submit</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
