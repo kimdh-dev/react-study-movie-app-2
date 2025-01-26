052062a1a8c9b87ff7a65bcf54d5a7f34b9b2581
kimdh <kdh.devs@gmail.com>
2025-01-26 18:28:14 +0900

    25/01/26 파라미터, 배포
    https://kimdh-dev.github.io/react-study-movie-app-2/

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState("");
+  const [genres, setGenres] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setTitle(json.data.movie.title);
+    setDetail(json.data.movie.description_full);
+    setBackgroundImage(json.data.movie.large_cover_image);
+    setGenres(json.data.movie.genres);
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      <div>title: {title}</div>
+      <div>detail: {detail}</div>
+      <div>
+        genres
+        <ul>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <img src={backgroundImage} />
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
